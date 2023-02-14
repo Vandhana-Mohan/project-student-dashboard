9e8381bb5b61525c35b47b18fa7e19fd4b4370cb
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-14 00:51:19 -0500

    header.css

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React from "react";
 import Students__data from "./data/data.json";
 import Header from "./Components/Header";
 import Student from "./Components/Student";
-
-
-
+import "./App.css"
 
 function App() {
   return (
-    <div>
+    <div className="main__container">
       <Header />
-      <main>
-            <h2>All Students </h2>
-            <h4>Total Students: {Students__data.length} </h4>
-            {Students__data.map((student) => {
-              return <Student student = {student}/>
-            })}
+      <main className="student__container">
+          <h2>All Students </h2>
+          <h4>Total Students: {Students__data.length} </h4>
+              {Students__data.map((student) => {
+                return <Student student = {student}/>
+              })}
       </main>
+      <aside className="cohort__container">
+        
+      </aside>
     </div>
   );
 }
--- src/Components/Student.js
+++ src/Components/Student.js
@@ -1,39 +1,29 @@
 
-const abbreviate__middleName = (name) => {
-    return name.charAt(0) + "." // abbreviate middle name
-}
+import "../Styles/Student.css"
 
-const format__dob = (dob) =>{
-    // console.log(dob, typeof dob)
-    const months = {
-                    '1': 'January',
-                    '2': 'February',
-                    '3': 'March',
-                    '4': 'April',
-                    '5': 'May', 
-                    '6': 'June',
-                    '7': 'July',
-                    '8': 'August',
-                    '9': 'September',
-                    '10': 'October',
-                    '11': 'November',
-                    '12': 'December'
-                    }
-        let split__date = dob.split('/')
-          
-        console.log(split__date, split__date[0])
-}
+const abbreviate__middleName = (name) => {
+  return name.charAt(0) + "."; // abbreviate middle name
+};
+
+const format__dob = (dob) => {
+  let split__date = dob.split("/");
+  const date = new Date();
+  date.setMonth(split__date[0] - 1);
+  return date.toLocaleString("en-US", { month: "long" }) + " " + split__date[1] + ", " + split__date[2];
+};
 
 const Student = (student) => {
-    
-    // console.log(student, student.student.names, student.student.dob)
-    return(
-        <div>
-            <h3>{student.student.names.preferredName} {abbreviate__middleName(student.student.names.middleName)} {student.student.names.surname} </h3>
-            <h5>{student.student.username}</h5>
-            <h5> Birthday: {format__dob(student.student.dob)}</h5>
-            <a href="#"> Show More...</a>
-        </div>
-    )
-}
-export default Student
+  return (
+    <div className="style__EachStudent">
+      <h3>
+        {student.student.names.preferredName}{" "}
+        {abbreviate__middleName(student.student.names.middleName)}{" "}
+        {student.student.names.surname}{" "}
+      </h3>
+      <h5>{student.student.username}</h5>
+      <h5> Birthday: {format__dob(student.student.dob)}</h5>
+      <a href="#"> Show More...</a>
+    </div>
+  );
+};
+export default Student;
