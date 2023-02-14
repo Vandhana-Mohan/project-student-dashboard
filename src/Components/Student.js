1b6efeb5d2a215c441c29c818263bbde2a10f752
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-13 21:36:17 -0500

    display student middle name initial

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -1,10 +1,16 @@
 
+const abbreviate__middleName = (name) => {
+    return name.charAt(0) + "." // abbreviate middle name
+}
+
+const expand__month = ()
 const Student = (student) => {
+    
     console.log(student, student.student.names, student.student.dob)
     return(
         <div>
-            <h3>{student.student.names.preferredName} {student.student.names.middleName} {student.student.names.surname} </h3>
-            <h5>{student.username}</h5>
+            <h3>{student.student.names.preferredName} {abbreviate__middleName(student.student.names.middleName)} {student.student.names.surname} </h3>
+            <h5>{student.student.username}</h5>
             <h5> Birthday: {student.student.dob}</h5>
             <a href="#"> Show More...</a>
         </div>
