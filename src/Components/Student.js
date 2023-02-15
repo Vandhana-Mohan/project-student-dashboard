8c34697d4bc4d9480d5b63391ee8516c57d1eb34
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-15 05:35:06 -0500

    student.css

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -13,16 +13,25 @@ const format__dob = (dob) => {
 };
 
 const Student = (student) => {
+  console.log(student)
   return (
     <div className="style__EachStudent">
-      <h3>
-        {student.student.names.preferredName}{" "}
-        {abbreviate__middleName(student.student.names.middleName)}{" "}
-        {student.student.names.surname}{" "}
-      </h3>
-      <h5>{student.student.username}</h5>
-      <h5> Birthday: {format__dob(student.student.dob)}</h5>
-      <a href="#"> Show More...</a>
+      <span>
+          <img className="student__pic" src={student.student.profilePhoto} alt={student.student.names.preferredName} />
+      </span>
+      <main className="style__EachStudent__info">
+        <h2>
+          {student.student.names.preferredName}{" "}
+          {abbreviate__middleName(student.student.names.middleName)}{" "}
+          {student.student.names.surname}{" "}
+        </h2>
+        <h5>{student.student.username}</h5>
+        <h5 className="last__h5"> 
+          <span className="style__EachStudent__dob">Birthday: </span>
+          <span>{format__dob(student.student.dob)}</span>
+        </h5>
+        <a href="#"> Show More...</a>
+      </main>
     </div>
   );
 };
