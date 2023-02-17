6f58a3ea78665a697b2798b2c511863685fa2d41
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-17 17:46:52 -0500

    edit app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import Header from "./Components/Header";
 import Student from "./Components/Student";
 import "./App.css"
 
+
 function App() {
+  const totalStudents = Students__data.length; // get total number of students
+
+  const studentList = Students__data.map((student) => { // map over the data and create a list of students
+    return <Student student={student} key={student.id} />; // specify a key for each student to identify them
+  });
+
   return (
     <div className="main__container">
       <div className="header"> 
@@ -15,10 +22,8 @@ function App() {
       </aside>
       <main className="student__container">
           <h2>All Students </h2>
-          <h4>Total Students: {Students__data.length} </h4>
-              {Students__data.map((student) => {
-                return <Student student = {student}/>
-              })}
+          <h4>Total Students: {totalStudents} </h4>
+              {studentList}
       </main>
       <div className="footer">
         Footer
