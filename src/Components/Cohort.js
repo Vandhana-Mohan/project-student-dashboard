831aba99dfd51d5c8defe7bfb87f2a9aad5ace81
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-18 03:27:41 -0500

    cohort button css

--- src/Components/Cohort.js
+++ src/Components/Cohort.js
@@ -2,12 +2,14 @@ import "../Styles/Cohort.css";
 
 const Cohort = ({ name, active, handleClick }) => {
   return (
+    <div>
     <button
       className={active ? "active__button" : "inactive__button"}
       onClick={handleClick}
     >
       {name}
     </button>
+    </div>
   );
 };
 
