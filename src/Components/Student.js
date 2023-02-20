6eea4cc96964d6a0a30d67d676854e8eaec255fb
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-19 20:56:28 -0500

    track-to-graduate.css

--- src/Components/Student.js
+++ src/Components/Student.js
@@ -2,10 +2,12 @@ import { useState } from "react";
 import "../Styles/Student.css";
 import StudentDetails from "./StudentDetails";
 
+// function to abbreviate middle name
 const abbreviate__middleName = (name) => {
-  return name.charAt(0) + "."; // abbreviate middle name
+  return name.charAt(0) + "."; 
 };
 
+// function to format date of birth
 const format__dob = (dob) => {
   const [month, day, year] = dob.split("/"); // Split the date of birth and destructure as array of month, day, and year strings.
   const date = new Date(year, month - 1, day); // Create new Date object from the array and - 1 for zero-based indexing.
@@ -13,6 +15,7 @@ const format__dob = (dob) => {
   return `${formattedMonth} ${day}, ${year}`; // Return the formatted date of birth using a template literal.
 };
 
+// function to check if a student is on track to graduate
 const isOnTrackToGraduate = (certifications, codewars) => {
   return (
     certifications.resume &&
@@ -26,13 +29,13 @@ const isOnTrackToGraduate = (certifications, codewars) => {
 const Student = ({ student }) => {
   // use destructuring to get the student object from the props
   const { profilePhoto, names, username, dob } = student; // use destructuring to get the relevant student properties
-  // console.log(student)
-  const [showDetails, setShowDetails] = useState(false);
-
+  
+  const [showDetails, setShowDetails] = useState(false); // state to toggle the display of the student details
+  // function to toggle the display of the student details
   const toggleDetails = () => {
     setShowDetails(!showDetails);
   };
-
+  // check if the student is on track to graduate
   const onTrackToGraduate = isOnTrackToGraduate(
     student.certifications,
     student.codewars
@@ -48,28 +51,33 @@ const Student = ({ student }) => {
         />
       </span>
       <main className="style__EachStudent__info">
+        {/* display student's name */}
         <h2>
-        <span className="style__EachStudent__green">Full Name: </span>
+          <span className="style__EachStudent__green">Full Name: </span>
           {names.preferredName} {abbreviate__middleName(names.middleName)}{" "}
           {names.surname}
         </h2>
+        {/* display student's graduation status */}
         <aside className="student__status">
-        <span className="style__EachStudent__green">
-          {onTrackToGraduate ? (
-            <p>On Track to Graduate</p>
-          ) : (
-            <p>Not Graduating</p>
-          )}
-        </span>
-      </aside>
+          <span className="style__EachStudent__green">
+            {onTrackToGraduate ? (
+              <p>On Track to Graduate</p>
+            ) : (
+              <p>Not Graduating</p>
+            )}
+          </span>
+        </aside>
+        {/* display student's email address */}
         <h5>
-        <span className="style__EachStudent__green">Email: </span>
-        {username}
+          <span className="style__EachStudent__green">Email: </span>
+          {username}
         </h5>
+        {/* display student's date of birth */}
         <h5 className="last__h5">
           <span className="style__EachStudent__green">Birthday: </span>
           <span>{format__dob(dob)}</span>
         </h5>
+        {/* display student's email address */}
         <button className="show__more" onClick={toggleDetails}>
           {" "}
           {showDetails ? "Show Less..." : "Show More..."}{" "}
