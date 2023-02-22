9cf7300dc3121834491d2d1de81e9e7bf216c666
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-22 03:57:08 -0500

    align cohort.js

--- src/Components/Cohort.js
+++ src/Components/Cohort.js
@@ -1,16 +1,17 @@
-    import "../Styles/Cohort.css";
 
-    const Cohort = ({ name, active, handleClick }) => {
-        return (
-            <div>
-                <button
-                    className={active ? "active__button" : "inactive__button"}
-                    onClick={handleClick}
-                    >
-                    {name}
-                </button>
-            </div>
-        );
-    };
+import "../Styles/Cohort.css";
 
-    export default Cohort;
+const Cohort = ({ name, active, handleClick }) => {
+  return (
+    <div>
+      <button
+        className={active ? "active__button" : "inactive__button"}
+        onClick={handleClick}
+      >
+        {name}
+      </button>
+    </div>
+  );
+};
+
+export default Cohort;
